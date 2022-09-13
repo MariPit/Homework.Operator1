40c924ee55f119f4ec7b7a9d3f60233fd369f45e
Мария <mariya_piterskaya@ymail.com>
2022-09-13 11:57:30 +0300

    Выполнено ДЗ-2

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,30 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-        int age = 15;
+        int age = 32;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
-        }
-        if (age < 18) {
+        } else {
             System.out.println("Возраст совершеннолетия ещё не наступил. Тебе нужно немного подождать");
         }
 
         System.out.println("Задание 2");
-        int age2 = 18;
-        if (age2 >= 7 && age2 < 18) {
+        int age2 = 23;
+        if (age2 >= 7 && age2 < 18 && age2 >= 18 && age2 < 24) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (age2 >= 18 && age2 < 24) {
+        } else {
             System.out.println("Человек уже закончил школу");
         }
-        if (age2 >= 24) {
+        if (age2 >= 24)
             System.out.println("Человек окончил университет");
-        }
 
         System.out.println("Задание 3");
-        int passenger = 102;
+        int passenger = 10;
         if (passenger < 60) {
             System.out.println("В вагоне есть сидячие места");
-        }
-        if (passenger >= 60 && passenger < 102) {
+        } else if (passenger >= 60 && passenger < 102) {
             System.out.println("В вагоне есть стоячие места");
-        }
-        if (passenger >= 102) {
+        } else {
             System.out.println("Вагон уже полностью забит");
         }
     }
